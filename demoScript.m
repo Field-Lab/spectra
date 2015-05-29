a2f3c8816b58a0ee63c2b5f3e620409d024179b1
DasVinch <vincent.deo.11@gmail.com>
2015-05-29 16:03:20 -0700

    added init neuron file - added demoScript

---
+++ demoScript.m
@@ -0,0 +1,7 @@
+%%%%%%% Demo script %%%%%%%%%
+% This script demonstrates the data flow in mVision
+% Starting at raw data files and ending at .neurons file.
+%
+% Work in progress
+% Current state: clustering not implemented
+% writing dummy clusters into .neurons
