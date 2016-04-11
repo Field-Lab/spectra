4c29c4c13582db9fb09cf1042bce9195ffd274e2
DasVinch <vincent.deo.11@gmail.com>
2016-04-11 15:29:48 +0200

    Previous function reoriented to work on final .neurons

--- util/corrValFunction.m
+++ util/corrValFunction.m
@@ -1,16 +1,19 @@
 function CC = corrValFunction( dataFolder, datasetName )
     %CORRVALFUNCTION Summary of this function goes here
     %   Detailed explanation goes here
-    neurPath = [dataFolder,filesep,datasetName,'.neurons.mat'];
+    neurPath = [dataFolder,filesep,datasetName,'.neurons'];
     corrPath = [dataFolder,filesep,datasetName,'.corr.mat'];
-    load(neurPath);
-    CC = eye(numel(neuronSpikeTimes));
-    for i = 1:numel(neuronSpikeTimes)
-        for j = (i+1):numel(neuronSpikeTimes)
-            CC(i,j) = edu.ucsc.neurobiology.vision.anf.NeuronCleaning.getCorrVal(neuronSpikeTimes{i},neuronSpikeTimes{j},10);
+    nrf = edu.ucsc.neurobiology.vision.io.NeuronFile(neurPath);
+    IDList = nrf.getIDList();
+    
+    CC = eye(numel(IDList));
+    for i = 1:numel(IDList)
+        for j = (i+1):numel(IDList)
+            CC(i,j) = edu.ucsc.neurobiology.vision.anf.NeuronCleaning.getCorrVal(nrf,IDList(i),IDList(j),10);
             CC(j,i) = CC(i,j);
         end
     end
     save(corrPath,'CC');
+    nrf.close();
 end
 
